fd49c752830fd0f11112c21388b5369eca5edd80
daviesjack083 <daviesjack083@gmail.com>
2025-02-05 12:17:28 +0000

    Removed junk files

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ A real time chat application written in C# I wrote some time ago when beginning 
 ## To-Do
 - Store the encryption key in code no longer -_-
 - Transition simple threading to an asynchronous/thread pool solution
+- Add a graceful shutdown for the server class
 - Improve upon the basic error handling
 - Add colours
 - Add rooms
--- Server/Server.cs
+++ Server/Server.cs
@@ -10,7 +10,20 @@ public class Server
 {
     Encryption _encrypt = new Encryption();
 
+    private bool isRunning = false;
     public static List<User> Users = new();
+
+
+    public static void Main()
+    {
+        Server server = new Server();
+        
+        // Shutdown server when CTRL+C is pressed
+        Console.CancelKeyPress += delegate {
+            server.Shutdown();
+        };
+        server.Start();
+    }
 
 
     public void Start()
@@ -25,8 +38,9 @@ public class Server
         socket.Listen(10);
 
         // Listen for connections
+        isRunning = true;
         LogEvent(String.Format("Server is now live on {0}", serverHost));
-        while (true) {        
+        while (this.isRunning) {        
             Socket clientSocket = socket.Accept();
             User user = new User(clientSocket);
             Users.Add(user);
@@ -41,7 +55,7 @@ public class Server
     private void Listen(User user)
     {
         Speak(String.Format($"{user.Ip} has joined! Connected Clients: {Users.Count()}"));
-        while (true)
+        while (this.isRunning)
         {
             byte[] bytes = new Byte[1024];
             if (user.Socket.Poll(200000, SelectMode.SelectRead))
@@ -65,6 +79,7 @@ public class Server
                 } else {
                     // Broadcast message
                     Users.ForEach(delegate(User user1) { user1.Socket.Send(Encoding.UTF8.GetBytes(message)); });
+                    //Speak(message);
                 }
             }
         }
@@ -74,9 +89,11 @@ public class Server
     public void Speak(String message)
     {
         LogEvent(message);
-        IDictionary<String, String> payload = new Dictionary<String, String>();
-        payload.Add("username", "[*]Server");
-        payload.Add("message", message);
+        IDictionary<String, String> payload = new Dictionary<String, String>
+        {
+            { "username", "[*]Server" },
+            { "message", message }
+        };
 
         DateTime currentDateTime = DateTime.Now;
         payload.Add("sent", currentDateTime.ToString("HH:mm:ss"));
@@ -94,9 +111,11 @@ public class Server
     public void Speak(String message, Socket client)
     {
         LogEvent(message);
-        IDictionary<String, String> payload = new Dictionary<String, String>();
-        payload.Add("username", "[*]Server");
-        payload.Add("message", message);
+        IDictionary<String, String> payload = new Dictionary<String, String>
+        {
+            { "username", "[*]Server" },
+            { "message", message }
+        };
 
         DateTime currentDateTime = DateTime.Now;
         payload.Add("sent", currentDateTime.ToString("HH:mm:ss"));
@@ -129,6 +148,14 @@ public class Server
         DateTime currentTime = DateTime.Now;
         string log = String.Format("{0} - {1}", currentTime.ToString("HH:mm:ss"), message);
         Console.WriteLine(log);
+    }
+
+
+
+    public void Shutdown()
+    {
+        LogEvent("Shutting down server...");
+        isRunning = false;
     }
     
 }
